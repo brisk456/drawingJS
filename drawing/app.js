3fc42f4a7e3240c54a6568fcc20895145a4e442c
brisk456 <brisk456@naver.com>
2023-05-01 18:08:45 +0900

    docs: 코드 파일 변경
    코드 파일 수정

--- drawing/app.js
+++ drawing/app.js
@@ -1,104 +1,153 @@
-const canvas = document.getElementById("canvas");
+const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
-const colors = document.getElementsByClassName("pen-color");
-const range = document.getElementById("range");
-const mode = document.getElementById("modeBtn");
-const saveBtn = document.getElementById("saveBtn");
 
 const INITIAL_COLOR = "#2c2c2c";
 const CANVAS_WIDTH = canvas.offsetWidth;
 const CANVAS_HEIGHT = canvas.offsetHeight;
 
-canvas.width = CANVAS_WIDTH;
-canvas.height = CANVAS_HEIGHT;
+const drawingOption = (() => { 
+    let isPainting = false;
+    let isFilling = "Draw";
 
-ctx.fillStyle = "white";
-ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-ctx.strokeStyle = INITIAL_COLOR;
-ctx.lineWidth = 2.5;
-ctx.fillStyle = INITIAL_COLOR;
+    return {
+        getIsPainting: () => {
+            return isPainting;
+        },
+        setIsPainting: (b) => {
+            isPainting = b;
+        },
+        getIsFilling: () => {
+            return isFilling;
+        },
+        setIsFilling: (b) => {
+            isFilling = b;
+        },
+    };
+})();
 
-let painting = false;
-let filling = false;
+document.addEventListener("DOMContentLoaded", () => {
+    const doc = document;
+    const lineWidth = doc.querySelector('#line-width');
 
-function stopPainting() {
-    painting = false;
+    canvas.width = CANVAS_WIDTH;
+    canvas.height = CANVAS_HEIGHT;
+
+    ctx.lineWidth = lineWidth.value;
+    ctx.lineCap = "round";
+
+    canvas.addEventListener("mousemove", onDrawing);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", onCanvasClick);
+    canvas.addEventListener("dblclick", onDoubleClick);
+    canvas.addEventListener("contextmenu", onPreventRight);
+
+    lineWidth.addEventListener("input", onRangeChange);
+    doc.querySelector('#mode-btn').addEventListener("click", onModeChange);
+    doc.querySelector('#destroy-btn').addEventListener("click", onDestroy);
+    doc.querySelector('#eraser-btn').addEventListener("click", onEraser);
+    doc.querySelector('#file').addEventListener("change", onFileChange);
+    doc.querySelector('#save-btn').addEventListener("click", onSave);
+
+    doc.querySelector('#color').addEventListener("change", onColorChange);
+    doc.querySelector('#colorOptions').addEventListener("click", onColorSelectChange);
+});
+
+function onDrawing(event) {
+    if (drawingOption.getIsPainting()) {
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+        return;
+    }
+
+    ctx.moveTo(event.offsetX, event.offsetY);
 }
 
 function startPainting() {
-    painting = true;
+    drawingOption.setIsPainting(true);
 }
 
-function onMouseMove(event) {
-    const x = event.offsetX;
-    const y = event.offsetY;
-    if (!painting) {
-        ctx.beginPath();
-        ctx.moveTo(x, y);
-    } else {
-        ctx.lineTo(x, y);
-        ctx.stroke();
+function stopPainting() {
+    drawingOption.setIsPainting(false);
+    if (drawingOption.getIsFilling() === "Bucket") {
+        ctx.fill();
     }
+    ctx.beginPath();
 }
 
-function handleColorClick(event) {
-    const color = event.target.style.backgroundColor;
-    ctx.strokeStyle = color;
-    ctx.fillStyle = color;
-}
-
-function handleRangeChange(event) {
-    const size = event.target.value;
-    ctx.lineWidth = size;
-}
-
-function handleModeChange(event) {
-    if (filling) {
-        filling = false;
-        mode.innerText = "Fill";
-    } else {
-        filling = true;
-        mode.innerText = "Paint";
-    }
-}
-
-function handleCanvesClick(event) {
-    if (filling) {
+function onCanvasClick() {
+    if (drawingOption.getIsFilling() === "Fill") {
         ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     }
 }
 
-function handleCM(event) {
+function onDoubleClick(event) {
+    const text = document.querySelector("#text").value;
+    if (text) {
+      ctx.save();
+      ctx.lineWidth = 1;
+      ctx.font = "68px sans-serif";
+      ctx.fillText(text, event.offsetX, event.offsetY);
+      ctx.restore();
+    }
+}
+
+function onPreventRight(event) {
     event.preventDefault();
 }
 
-function handeSaveClick(event) {
-    const image = canvas.toDataURL("image/jpeg");
+function onColorChange(event) {
+    const color = event.target.value;
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+}
+
+function onColorSelectChange(event) {
+    const color = event.target.dataset.color;
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+
+    document.querySelector('#color').value = color;
+}
+
+function onRangeChange(event) {
+    ctx.lineWidth = event.target.value;
+}
+
+function onModeChange(event) {
+    const mode = event.target.innerText;
+    if (mode) {
+        drawingOption.setIsFilling(mode);
+    }
+}
+
+function onFileChange(event) {
+    const file = event.target.files[0];
+    const url = URL.createObjectURL(file);
+    const image = new Image();
+    image.src = url;
+    image.onload = () => {
+      ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+      document.getElementById("file").value = null;
+    };
+  }
+
+function onSave() {
+    const url = canvas.toDataURL();
     const link = document.createElement("a");
-    link.href = image;
-    link.download = "image";
+    link.href = url;
+    link.download = "drawing.png";
     link.click();
 }
 
-if (canvas) {
-    canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", startPainting);
-    canvas.addEventListener("mouseup", stopPainting);
-    canvas.addEventListener("mouseleave", stopPainting);
-    canvas.addEventListener("click", handleCanvesClick);
-    canvas.addEventListener("contextmenu", handleCM);
+function onDestroy() {
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 }
 
-Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
-
-if (range) {
-    range.addEventListener("input", handleRangeChange);
-}
-
-if (mode) {
-    mode.addEventListener("click", handleModeChange);
-}
-
-if (saveBtn) {
-    saveBtn.addEventListener("click", handeSaveClick);
+function onEraser() {
+    ctx.strokeStyle = "white";
+    drawingOption.setIsFilling("Draw");
+    document.querySelector("#draw-mode").checked = true;
 }
